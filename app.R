06e1d24d712cf886cc6665637cd7b01eb3b59b1c
fpirotti <fpirotti@gmail.com>
2020-12-27 16:29:15 +0100

    fixed multiple calls

---
+++ app.R
@@ -0,0 +1,309 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(readr)
+library(shinyalert)
+library(shinyWidgets)
+library(curl)      
+library(shinyjs)  
+library(httr)
+library(openxlsx)
+library(sf)
+# library(future)
+# library(promises)
+#source("functions.R")
+
+# plan(multisession)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    useShinyjs(),  # Include shinyjs
+    tags$head(
+      tags$style(HTML("
+      @import url('//fonts.googleapis.com/css?family=Lobster|Cabin:400,700');
+      .fontRed{
+        color:red;
+        font-weight:bold;
+      }
+      h4 {
+        font-family: 'Lobster', cursive;
+        font-weight: 500;
+        line-height: 1.1;
+        color: #48ca3b;
+      }
+
+    "))
+    ),
+    # Application title
+    titlePanel("CIRGEO reverse geocoding"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            fileInput("file1", label = h3("File input")),
+            fluidRow(
+                column(6, checkboxInput("header", div(title="File con prima riga con intestazione (nome colonne)",
+                                                      HTML("Header<sup>?</sup>")), TRUE) ) ,
+                column(6, checkboxInput("forceNonCache", 
+                                     div(title="Non utilizza la cache ma obbliga nuova interrogazione da Engine selezionata", 
+                                         HTML("Force<sup>?</sup>")), F) )
+            ),
+            numericInput("maxErrs", div(title="Maximum number of allowed errors - leave 0 for infinite",
+                                        HTML("Max Errors<sup>?</sup>") ), value = 0 ),
+            shiny::actionButton("process", "START"),
+            downloadButton('downloadData', 'Download XLS', icon=icon("table")),
+            downloadButton('downloadData2', 'Download GPKG') 
+            
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            fluidRow(column(12, shinyWidgets::prettyRadioButtons(
+                "type", div(title="Una delle Engine di Geocoding - Att.ne Google a pagamento. 
+NB: 'ALL' applica in preferenza BING, poi HERE e in ultima istanza Google.", 
+                            HTML("ENGINE<sup>?</sup>"))
+                ,inline = T
+                ,list(
+                    google = "G",
+                    bing = "B",
+                    here = "H",
+                    ALL = "ALL"
+                ), selected = "ALL"
+            ))),
+            progressBar(id = "progBar", value = 0, total = 5000, status = "warning"),
+            
+            h5("Preview loaded address") ,
+            tableOutput("contents"),
+            fluidRow(column(3, h5(HTML("Error Log Table: 
+                                       tot errors <span id='tErrors'>0</span>") ) ), 
+                     column(9, downloadButton("downloadErrLog", "Download Error Table") ) ),
+            tableOutput("logTable")
+        )
+    )
+)
+
+
+
+# Define server logic required to draw a histogram
+server <- function(input, output, session) {
+    currentEngine<-"H"
+    currentFile<-NULL
+    currKey<-1
+    mytable<-NULL
+    stopIt<-F
+    isRunning<-F
+    
+    rVals<-reactiveValues(outFile=NULL,
+                          errTable=data.frame(Engine=character(0),Error=character(0),Address=character(0)) 
+                          )
+    errLog<-tempfile("log")
+    
+     
+    observeEvent(rVals$errTable, {
+      write_tsv(rVals$errTable,errLog)
+    })
+    
+    output$logTable <- renderTable({
+      rVals$errTable
+    })
+    
+    output$contents <- renderTable({ 
+        req(rVals$outFile)
+        rVals$outFile
+    })
+     
+    output$contents <- renderTable({ 
+        file <- input$file1
+        req(file)
+        ext <- tools::file_ext(file$datapath) 
+        
+        if(file.size(file$datapath)/1000000 > 10){
+            shinyWidgets::show_alert("File size is too big, please limit to 10 MB")
+            return(NULL)
+        }
+        
+        if( tolower(ext)=="zip"){
+            cf<-unzip(file$datapath, list  = T)
+            currentFile<<-list(name=cf[["Name"]], 
+                               size=cf[["Length"]])
+            unzip(file$datapath, overwrite = T)
+        }
+        else currentFile<<- file
+        
+        validate(need( is.element(tolower(ext),c("csv","zip", "txt","dat")), 
+                                  "Please upload a file zip or CSV or TXT extensions. Must be a table"))
+        
+        mytable<<-tryCatch({ 
+            read_tsv(file$datapath)
+        }, error=function(e){
+            return(e)
+        })
+        
+        if(ncol(mytable)!=1){
+            shinyWidgets::show_alert("Only one column with addresses please.")
+            return(NULL)
+        }
+        
+        # server
+        updateProgressBar(session = session, id = "progBar", value = 0, total = nrow(mytable))
+        mytable[1:4,]
+    })
+    
+    ### DOWNLOAD BUTTONS --------
+    output$downloadData <- downloadHandler(
+ 
+      filename = function() {
+        if(is.null(currentFile$name)){
+          shinyWidgets::show_alert("Non è stato ancora elaborato alcun dato")
+          return(NULL)
+        }
+        bn<-basename(currentFile$name)
+        raster::extension(bn)<-""
+        paste(bn, '_', isolate(input$type), '_',
+              format(Sys.Date(), "%Y%m%dT%I%M%S"), '.xlsx', sep='')
+      },
+      content = function(con) {
+           openxlsx::write.xlsx(isolate(rVals$outFile), con)
+      }
+    )
+    
+    output$downloadData2 <- downloadHandler(
+
+      filename = function() {
+        if(is.null(currentFile$name)){
+          shinyWidgets::show_alert("Non è stato ancora elaborato alcun dato")
+          return(NULL)
+        }
+        bn<-basename(currentFile$name)
+        raster::extension(bn)<-""
+        paste(bn, '_', isolate(input$type), '_',
+              format(Sys.Date(), "%Y%m%dT%I%M%S"), '.gpkg', sep='')
+      },
+      content = function(con) {  
+        sfobj<-st_as_sf(isolate(rVals$outFile), coords=c("long", "lat") )
+        st_crs(sfobj) = 4326
+        sf::write_sf(sfobj, con)
+      }
+    )
+    
+    output$downloadErrLog <- downloadHandler(
+      filename = function() {
+        if(is.null(currentFile$name)){
+          shinyWidgets::show_alert("Non è stato ancora elaborato alcun dato")
+          return(NULL)
+        }
+        bn<-basename(currentFile$name)
+        raster::extension(bn)<-""
+        paste(bn, '_', isolate(input$type), '_',
+              format(Sys.Date(), "%Y%m%dT%I%M%S_ErrorTable"), '.xlsx', sep='')
+      },
+      content = function(con) {
+        openxlsx::write.xlsx(isolate(rVals$errTable), con)
+      }
+    )
+    
+ 
+    
+    observeEvent(input$type, {
+      if(input$type=="ALL"){
+        currentEngine<<-"H"
+      } else {
+        currentEngine<<-input$type
+      }
+      print(currentEngine)
+    })
+    
+    
+    observeEvent(input$process, {
+        req(mytable) 
+      
+        if(isRunning){
+          shinyjs::html("process", HTML("<div onclick='Shiny.setInputValue(\"shouldStop\", true, {priority: \"event\"})'>RUNNING!</div>") )
+          shinyWidgets::show_alert("Process already running")
+          return(NULL)
+        }   
+      
+        isRunning<-T
+      
+        resultTable<-list()
+ 
+        ## each row in table
+        ttrows<-nrow(mytable)
+         
+        totErrs<-0
+        
+        shinyjs::html("process", HTML("<div onclick='Shiny.setInputValue(\"shouldStop\", true, {priority: \"event\"})'>RUNNING!</div>") )
+ 
+        #future({  --- check http://blog.fellstat.com/?p=407
+        
+          for(i in 1:ttrows){
+            address<-mytable[i,1][[1]] 
+            if(is.null(cacheFile[[currentEngine]][[address]]) || isolate(input$forceNonCache) ){
+
+                pC<- parseContent(isolate(input$type), address)
+              
+                if(is.list(pC)) {
+                    resultTable[[address]]<-pC
+                } else {
+                    totErrs<-totErrs+1
+                    shinyjs::html("tErrors", as.character(totErrs))
+                    shinyjs::addClass("tErrors", "fontRed")
+                    
+                    if(isolate(input$maxErrs) > 0 && totErrs>isolate(input$maxErrs)){
+                        shinyWidgets::show_alert(sprintf("Too many errors, aborting. 
+                                                 Last error is: '%s'", pC))
+                        saveRDS(cacheFile, "cacheFile.rds")
+                        return(NULL)
+                    }
+                    
+                    resultTable[[address]]<- list(
+                        lat=0,
+                        long=0,
+                        accur=pC,
+                        formattedAddress="NA",
+                        type = currentEngine
+                    )
+                }
+                
+                updateProgressBar(session = session, id = "progBar", value = i, total = ttrows )   
+                
+            } else { 
+                resultTable[[i]] <- cacheFile[[currentEngine]][[address]]
+            } 
+
+        
+            if(i%%as.integer(ttrows/200)==0){ 
+              updateProgressBar(session = session, id = "progBar", value = i, total = ttrows )
+               
+            }
+          }
+
+          saveRDS(cacheFile, "cacheFile.rds")
+          tt <- data.table::rbindlist(resultTable )
+          
+          tt$Original_Address<-mytable[,1]
+          
+          print(length(unique(unlist(mytable[,1]))))
+          
+          rVals$outFile<-tt
+          isRunning<-F
+          updateProgressBar(session = session, id = "progBar", value = ttrows, total = ttrows )   
+          shinyjs::html("process", "START")
+          
+      #  })
+
+        
+
+        
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
--- functions.R
+++ functions.R
@@ -1,7 +1,103 @@
-decode.bing<-function(content){
-  if(content$statusCode!=200){
+# ovreallGcalls<-list()
+# ovreallGcalls[[format(Sys.time(), "%Y%m")]]<-0
+# saveRDS(ovreallGcalls,"ovreallGcalls.rds")
+
+if(file.exists("cacheFile.rds")) {
+  cacheFile<-readRDS("cacheFile.rds")
+} else {
+  cacheFile<-list(bing=list(), google=list(), here=list())
+}
+
+ovreallGcalls<-readRDS("ovreallGcalls.rds")
+
+keys <- list(
+  G = c(
+    "AIzaSyDvfSqURa60jKdXBEcL7hnwsvwEWhYgkm4",
+    "AIzaSyDeAaUewDUcIriQsqOX1NNGxatX8WkbZIE",
+    "AIzaSyCMV7dLRGHwkTyuOhojM03Sgh5Y3IZAGXM",
+    "AIzaSyCpQoUQ9Pnz3jzCE8KpRWW1bBuzT-_a97M"
+  ),
+  
+  B = c(
+    "AqfXHrbz_Qm4PuVk7lAPMA7ue1vuhMbJgLI-3ZwGBQ3QRIqpMcMBDx2N6OZdJYoj",
+    "At2br5wQAqbVaQW6Fn9eJTTbnrMiaSkriO4lx6YdfDSGNsGI3lMYMYYjlmCbjU-5",
+    "AtFnmH3kI3H_eOuB1eyxEd66fISOnuj6R7nuM_uhPAhtB7_yL1bTr0Wt4gj_ZVf8",
+    "AjvjPYuoA4IgNeooKvodDLcxbVL1F8RdIxXUeYsb6PgiVapURz_PbbWvOxVKmNps"
+  ),
+  
+  H = c("YeoIkazjfVa2WND4PUvLAjDR7VGRV_QZnfQ80TjTZSc")
+)
+API.list <-
+  list(
+    G = "https://maps.googleapis.com/maps/api/geocode/json?key=%s&region=IT&address=%s",
+    B = "http://dev.virtualearth.net/REST/v1/Locations?CountryRegion=IT&key=%s&addressLine=%s",
+    B2 = "https://www.bing.com/maps/overlay?q=%s",
+    H = "https://geocode.search.hereapi.com/v1/geocode?limit=1&in=countryCode:ITA&apiKey=%s&q=%s"
+  )
+
+
+
+
+readURL<-function(url){
+
+  r <- GET((url))
+  if(status_code(r)!=200){
+    shinyWidgets::show_alert(sprintf("Errore. Codice %s:    ---  %s",
+                                     status_code(r), http_status(r)$message,
+                                     url )) 
     return(NULL)
   }
+  
+  content(r, "parsed")
+}
+
+decode.functions<-list()
+
+
+decode.here<-function(content){
+  if(!is.null(content$error)){
+    return(content$error_description)
+  }
+  
+  if(length(content$items)==0){
+    return( list(lat=0,
+                 long=0,  
+                 accur="NA",
+                 formattedAddress="NA", 
+                 type="H") )   
+  }
+  
+  latlng<-unlist(content$items[[1]]$position)
+  accur<-content$items[[1]]$resultType
+  formattedAddress<-content$items[[1]]$address$label
+  
+  list(lat=latlng[[1]],
+       long=latlng[[2]],
+       accur=accur,
+       formattedAddress=formattedAddress, 
+       type="H")
+}
+
+
+decode.bing<-function(content){
+  
+  if(content$statusCode!=200){ 
+    return(as.character(content$statusDescription))
+  }
+  
+  if(length(content$resourceSets)==0){
+    return( list(lat=NA,
+                 long=NA,  accur=NA,
+                 formattedAddress=NA, type="B") )   
+  }
+  
+  
+  if(length(content$resourceSets[[1]]$resources)==0){
+    return( list(lat=NA,
+                 long=NA,  accur=NA,
+                 formattedAddress=NA, type="B") )   
+  }
+  
   latlng<-unlist(content$resourceSets[[1]]$resources[[1]]$point$coordinates)
   accur<-content$resourceSets[[1]]$resources[[1]]$confidence
   formattedAddress<-content$resourceSets[[1]]$resources[[1]]$address$formattedAddress
@@ -13,10 +109,18 @@ decode.bing<-function(content){
        type="B")
 }
 
+
 decode.google<-function(content){
   if(content$status!="OK"){
     return(content$status)
   }
+  
+  if(length(content$results)==0){
+    return( list(lat=0,
+                 long=0,  accur="NA",
+                 formattedAddress="NA", type="G") )   
+  }
+  
   latlng<-unlist(content$results[[1]]$geometry$location)
   accur<-content$results[[1]]$geometry$location_type
   formattedAddress<-content$results[[1]]$formatted_address
@@ -27,37 +131,71 @@ decode.google<-function(content){
        formattedAddress=formattedAddress, type="G")
 }
 
-decode.here<-function(content){
-  if(!is.null(content$error)){
-    return(content$error_description)
-  }
 
+decode.functions[["B"]]<-decode.bing
+decode.functions[["H"]]<-decode.here
+decode.functions[["G"]]<-decode.google
   
-  latlng<-unlist(content$items[[1]]$position)
-  accur<-content$items[[1]]$resultType
-  formattedAddress<-content$items[[1]]$address$label
-  
-  list(lat=latlng[[1]],
-       long=latlng[[2]],
-       accur=accur,
-       formattedAddress=formattedAddress, type="H")
+getContent<-function(engine, address){
+  url<-sprintf(API.list[[engine]], 
+               keys[[engine]][[1]],
+               URLencode(address) )
+  readURL(url) 
 }
 
-
-
-parseContent<-function(content, type){
+parseContent<-function( type, address){
   
-  if(type=="bing"){
-    dd<-decode.bing(content)
-  } 
-  else if(type=="google"){
-    dd<-decode.google(content)
-  }
-  else if(type=="here"){
-    dd<-decode.here(content)
+  if(type=="ALL"){
+    for(f in names(decode.functions)){
+      
+      if(f=="G"){
+        #### verifica che google non sia troppo sfruttato ------
+        tt<-format(Sys.time(), "%Y%m")
+        if(is.null(ovreallGcalls[[format(Sys.time(), "%Y%m")]])) {
+          ovreallGcalls[[tt]]<<-0
+        } else {
+          ovreallGcalls[[tt]]<<- ovreallGcalls[[tt]]+1
+        } 
+        print(paste("Arrivato a Google:", content, " N.", ovreallGcalls[[tt]]))
+        if(ovreallGcalls[[tt]]>100){
+          
+          shinyWidgets::show_alert( "Troppo Google questo mese") 
+          return(NULL)
+        }
+      }
+      
+      content<-getContent(f, address)
+      
+      if(is.null(content)){
+        next
+      }
+      
+      dd<-decode.functions[[f]](content)
+      
+      if(is.list(dd)){
+        cacheFile[[dd$type]][[address]]<<-dd
+        if(length(cacheFile[[dd$type]])%%100==0) {
+          print("Saving cache")
+          saveRDS(cacheFile, "cacheFile.rds")
+        }
+      }
+      if(is.list(dd) &&  shiny::isTruthy(dd$lat) ){
+        #cacheFile[[dd$type]][[address]]<<-dd
+        break
+      } 
+    }
   } else {
-    return(NULL)
+    content<-getContent(type, address)
+    dd<-decode.functions[[type]](content)
+    if(is.list(dd)){
+      cacheFile[[dd$type]][[address]]<<-dd
+      if(length(cacheFile[[dd$type]])%%100==0) {
+        print("Saving cache")
+        saveRDS(cacheFile, "cacheFile.rds")
+      }
+    }
   }
   
   dd
+  
 }
